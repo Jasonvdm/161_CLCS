67cff8d964d3953e603dc47cb36f4b813c35ed64
bridge-eimon <beimon@stanford.edu>
2014-06-02 09:37:45 -0700

    Works on my laptop

--- project/CLCSFast.java
+++ project/CLCSFast.java
@@ -32,7 +32,7 @@ public class CLCSFast {
 		}
 	}
 
-	private static Path FirstPath() {
+	private static void FirstPath() {
 		int m = A.length(), n = B.length();
 		int i, j;
 		for (i = 0; i <= m; i++) DP[i][0] = 0;
@@ -46,9 +46,26 @@ public class CLCSFast {
 		}
 		Path p = new Path(m+1,n+1);
 		p.setScore(DP[m][n]);
+		int row = m;
+		int col = n;
+		while(row > 0 && col > 0){
+			p.addNode(col,row);
+			if(row == 0) col--;
+			else if(col == 0) row--;
+			else{
+				int l = DP[row][col-1];
+				int up = DP[row-1][col];
+				int d = DP[row-1][col-1];
+				int c = DP[row][col];
+				if (d == c-1 && A.charAt(row-1) == B.charAt(col-1)){
+					row--;
+					col--;
+				}
+				else if (up == c) row--;
+				else if (l == c) col--;
+			}
+		}
 		paths[0] = p;
-		getPath(m,n,0,0,0,true);
-		return p;
 	}
 
 	private static void SingleShortestPath(int mid, int lower, int upper) {
@@ -120,7 +137,7 @@ public class CLCSFast {
 			}
 			DP = new int[A.length()+1][B.length()+1];
 			paths = new Path[A.length()+1];
-			paths[0] = FirstPath();
+			FirstPath();
 			paths[A.length()] = paths[0];
 			FindShortestPaths(0,A.length());
 			int maxLength = -1;
